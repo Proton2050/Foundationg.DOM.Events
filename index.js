63cad7dc77c717a26f3bcd0d1e9402850b6df0f9
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-05-03 14:47:49 -0700

    magnifyer created

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ cookieButton.addEventListener('click', function() {
 const container = document.getElementById('counter');
 container.style.backgroundColor = "red";
 // container.style.backgroundImage = "url('cookie.jpeg')";
+
+// Magnify Text
+const bigger = document.getElementById('bigger');
+const text = document.querySelector('p');
+const smaller = document.getElementById('smaller');
+let currentFontSize = parseInt(window.getComputedStyle(text).fontSize);
+bigger.addEventListener('click', function() {
+    if (currentFontSize >= 100) return;
+    currentFontSize++;
+    text.style.fontSize = currentFontSize + 'px';
+    console.log(`Current size ${currentFontSize}`);
+    console.log('Clicked!');
+    // let fontSize = parseInt(text.style.fontSize);
+    // console.log(`current size: ${fontSize}`)
+    
+});
+smaller.addEventListener('click', function () {
+    if (currentFontSize <= 1) return;
+    currentFontSize--;
+    text.style.fontSize = currentFontSize + 'px';
+    console.log(`Current size ${currentFontSize}`);
+    console.log('Clicked!');
+})
